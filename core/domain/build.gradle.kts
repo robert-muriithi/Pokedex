6a3f789d393727ac925462fbca4bffec41fd196f
robert <njuguiinirobert@gmail.com>
2026-01-14 14:40:57 +0300

    create launcher icon

--- core/domain/build.gradle.kts
+++ core/domain/build.gradle.kts
@@ -12,6 +12,10 @@ android {
         version = release(36)
     }
 
+    defaultConfig {
+        minSdk = 24
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
