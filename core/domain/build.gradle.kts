331f6e7da409d246e2ea7b11879c8d4ec0026c4b
robert <njuguiinirobert@gmail.com>
2026-01-14 14:08:55 +0300

    clean dependencies

--- core/domain/build.gradle.kts
+++ core/domain/build.gradle.kts
@@ -12,10 +12,6 @@ android {
         version = release(36)
     }
 
-    defaultConfig {
-        minSdk = 24
-    }
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
--- core/navigation/build.gradle.kts
+++ core/navigation/build.gradle.kts
@@ -10,13 +10,6 @@ android {
         version = release(36)
     }
 
-    defaultConfig {
-        minSdk = 24
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
